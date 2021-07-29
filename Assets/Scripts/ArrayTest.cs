f228a9a9944a4cd603205a91ff1a113af78971b2
wowdud <tim.heybatov@gmail.com>
2021-07-29 13:05:38 +1000

    Array test
    Renamed circle to Traveller for clarity

---
+++ Assets/Scripts/ArrayTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayTest : MonoBehaviour
+{
+    public string[] stringArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(stringArray[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
