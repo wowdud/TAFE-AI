04c2d2230aabc3ae8cc509174f22d46a08968ab2
wowdud <tim.heybatov@gmail.com>
2021-07-29 11:49:10 +1000

    Updated waypoints to add list and looping

--- Assets/Scripts/WaypointAI.cs
+++ Assets/Scripts/WaypointAI.cs
@@ -11,22 +11,24 @@ public class WaypointAI : MonoBehaviour
     public float speedY = 1f;
     public GameObject waypoint;
     public GameObject waypoint2;
+    public GameObject waypoint3;
+    public GameObject waypoint4;
     public GameObject CurrentGoal;
-    public int waypointCount; 
-    
-    
+    public int listPos = 0;
+
+    List<GameObject> pointList = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        List<GameObject> pointList = new List<GameObject>();
-        CurrentGoal = waypoint;
-        while (waypointCount > 0)
-        {
-            pointList.Add(CurrentGoal);
-            waypointCount -= 1;
-        }
-        
+
+
+        pointList.Add(waypoint);
+        pointList.Add(waypoint2);
+        pointList.Add(waypoint3);
+        pointList.Add(waypoint4);
+
+        CurrentGoal = pointList[0];
     }
 
     // Update is called once per frame
@@ -34,13 +36,13 @@ public class WaypointAI : MonoBehaviour
     {
 
         float distance = Vector2.Distance(transform.position, CurrentGoal.transform.position);
-  
+
 
         //float distance = (waypoint.transform.position - transform.position).magnitude;
-        
+
 
         if (distance >= 0.01f)
-            {
+        {
             Vector2 direction = CurrentGoal.transform.position - transform.position;
             direction.Normalize();
 
@@ -48,11 +50,19 @@ public class WaypointAI : MonoBehaviour
             aiPosition.x += direction.x * speedX * Time.deltaTime;
             aiPosition.y += direction.y * speedY * Time.deltaTime;
             transform.position = aiPosition;
-            }
+        }
         else
         {
-            CurrentGoal = waypoint2;
+            listPos = listPos + 1;
+            if (listPos < pointList.Count)
+            {
+                CurrentGoal = pointList[0 + listPos];
+            }
+            else
+            {
+                CurrentGoal = pointList[0];
+                listPos = -1;
+            }
         }
-
     }
 }
